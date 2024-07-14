53b71f40d7556d0eae8fb020311ffef884af4e1b
Santiago MAC <spg1824@gmail.com>
2024-07-14 11:04:06 -0600

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import { useState, useRef  } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+
+
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+
+
+
 
 const Contact = () => {
   return (
